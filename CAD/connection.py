32ae881e7d790e0b246cce86349ec8aed82821bf
samanthalz <samanthalz1610@gmail.com>
2024-06-06 13:27:08 +0800

    login draft + db connection

--- CAD/connection.py
+++ CAD/connection.py
@@ -5,4 +5,4 @@ from firebase_admin import db
 cred = credentials.Certificate("CAD\call-a-doctor-bbd15-firebase-adminsdk-pfmoy-2b811d48ca.json")
 firebase_admin.initialize_app(cred)
 
-ref = db.reference('Database reference')
+ref = db.reference('/')
