f5013695d3093ba867a56af57e1be4719ecbb7f6
Lee Yan <amber.leeyan04@gmail.com>
2024-06-14 11:54:23 +0800

    success page for forgotpw

---
+++ CAD/User/forgotpw_success.py
@@ -0,0 +1,120 @@
+from PyQt5.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PyQt5.QtWidgets import *
+
+class ForgotPw_newpwWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+    def setupUi(self, Form):
+        if Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(1920, 1080)
+        self.bg = QFrame(Form)
+        self.bg.setObjectName(u"bg")
+        self.bg.setGeometry(QRect(0, 0, 1920, 1080))
+        self.bg.setMinimumSize(QSize(1920, 1080))
+        self.bg.setFrameShape(QFrame.StyledPanel)
+        self.bg.setFrameShadow(QFrame.Raised)
+        self.widget_2 = QWidget(self.bg)
+        self.widget_2.setObjectName(u"widget_2")
+        self.widget_2.setGeometry(QRect(0, 0, 1350, 1080))
+        self.widget_2.setMinimumSize(QSize(0, 1080))
+        self.widget_2.setStyleSheet(u"background-color: \"white\";")
+        self.widget = QWidget(self.widget_2)
+        self.widget.setObjectName(u"widget")
+        self.widget.setEnabled(True)
+        self.widget.setGeometry(QRect(550, 540, 658, 248))
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.widget.sizePolicy().hasHeightForWidth())
+        self.widget.setSizePolicy(sizePolicy)
+        self.widget.setStyleSheet(u"")
+        self.verticalLayout = QVBoxLayout(self.widget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.label = QLabel(self.widget)
+        self.label.setObjectName(u"label")
+        self.label.setEnabled(True)
+        font = QFont()
+        font.setFamily(u"Consolas")
+        font.setPointSize(28)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setStyleSheet(u"color:rgb(21, 48, 96)")
+        self.label.setAlignment(Qt.AlignHCenter|Qt.AlignTop)
+
+        self.verticalLayout.addWidget(self.label)
+
+        self.verticalSpacer_3 = QSpacerItem(640, 44, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer_3)
+
+        self.label_2 = QLabel(self.widget)
+        self.label_2.setObjectName(u"label_2")
+        font1 = QFont()
+        font1.setFamily(u"Consolas")
+        font1.setPointSize(12)
+        self.label_2.setFont(font1)
+        self.label_2.setStyleSheet(u"color: rgb(130, 130, 130);")
+        self.label_2.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_2)
+
+        self.verticalSpacer_5 = QSpacerItem(14, 44, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer_5)
+
+        self.Con_Login = QPushButton(self.widget)
+        self.Con_Login.setObjectName(u"Con_Login")
+        self.Con_Login.setMinimumSize(QSize(0, 40))
+        self.Con_Login.setFont(font1)
+        self.Con_Login.setStyleSheet(u"background-color: rgb(182, 208, 226);\n"
+"border-radius: 10px;\n"
+"border: none\n"
+"")
+
+        self.verticalLayout.addWidget(self.Con_Login)
+
+        self.label_7 = QLabel(self.widget_2)
+        self.label_7.setObjectName(u"label_7")
+        self.label_7.setGeometry(QRect(540, 270, 681, 531))
+        self.label_7.setStyleSheet(u"border: 2px solid rgb(182, 208, 226);\n"
+"border-radius: 10px;")
+        self.label_3 = QLabel(self.widget_2)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setEnabled(True)
+        self.label_3.setGeometry(QRect(730, 280, 301, 261))
+        self.label_3.setPixmap(QPixmap(u"../Images/icon/Successful_icon.png"))
+        self.label_3.setScaledContents(True)
+        self.label_3.setAlignment(Qt.AlignCenter)
+        self.label_3.setWordWrap(False)
+        self.label_7.raise_()
+        self.widget.raise_()
+        self.label_3.raise_()
+        self.widget_3 = QWidget(self.bg)
+        self.widget_3.setObjectName(u"widget_3")
+        self.widget_3.setGeometry(QRect(1348, 0, 579, 1080))
+        self.widget_3.setMinimumSize(QSize(0, 1080))
+        self.widget_3.setStyleSheet(u"background-color: \"#B6D0E2\";")
+        self.widget_3.raise_()
+        self.widget_2.raise_()
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.label.setText(QCoreApplication.translate("Form", u"Successfully", None))
+        self.label_2.setText(QCoreApplication.translate("Form", u"Your password has been reset successfully", None))
+        self.Con_Login.setText(QCoreApplication.translate("Form", u"Continue", None))
+        self.label_7.setText("")
+        self.label_3.setText("")
+    # retranslateUi
+
